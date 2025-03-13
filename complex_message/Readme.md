b6ec705b1f2ec470a88b5163707e8bcc7688126b
Mohammed Sajadh <sajadhaja90@gmail.com>
2025-03-13 21:41:30 +0100

    Add complex image sending with caption or message

---
+++ complex_message/Readme.md
@@ -0,0 +1,10 @@
+#Setup python Environment
+python -m venv .venv
+
+.venv\Scripts\activate
+
+pip install pywhatkit
+pip install pandas
+pip install pywin32
+
+cd complex_message
--- complex_message/send_complex_message.py
+++ complex_message/send_complex_message.py
@@ -1,12 +1,49 @@
+import pandas as pd
+import urllib
+import time
 import pywhatkit as kit
-# Send a WhatsApp message
-phone_number = "+1234567890"  # Replace with recipient's phone number (include country code)
-message = "Hello! This is an automated message sent using Python."
-hour = 14  # Specify the hour in 24-hour format
-minute = 30  # Specify the minute
 
-try:
-    kit.sendwhatmsg_instantly(phone_number, message)
-    print("Message scheduled successfully!")
-except Exception as e:
-    print(f"An error occurred: {e}")
+#Read CSV for phone numbers
+def read_phone_numbers():
+    csv_file = "phone_numbers.csv"
+    df = pd.read_csv(csv_file, header=None)  # No headers in CSV
+    return df.iloc[:, 0].astype(str).tolist()  # Convert to list of strings
+
+# Load message from text file
+def read_message():
+    txt_file = "message_template.txt"
+    with open(txt_file, "r", encoding="utf-8") as file:
+        return file.read()  # Read and remove extra spaces/newlines
+
+numbers = read_phone_numbers()
+message = read_message()
+# Encode message for URL
+encoded_message = urllib.parse.quote(message)
+
+def bot_send_message():
+    for number in numbers:
+        try: 
+            #Caption image
+            kit.sendwhats_image(
+                receiver=number.strip(),  # Phone number
+                img_path="kmcc.jpeg",  # Path to the image file
+                close_time=10,
+                tab_close=True
+            )
+            time.sleep(5)
+            kit.sendwhatmsg_instantly(
+                phone_no=number, 
+                message=message,
+                close_time=10,
+                tab_close=True
+            )
+            time.sleep(5)
+            #Schedule mesage
+            #kit.sendwhatmsg(contact, message, hour, minute)
+            print(f"Message sent to {number}")
+        except Exception as ex:
+            print(f"Failed to send message to {number}. Reason : {ex}")
+
+
+
+bot_send_message()
